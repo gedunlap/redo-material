19f75b5d559b9a5535626e5854695d3af68cd83e
gedunlap <g.e.dunlap90@gmail.com>
2023-01-02 17:18:50 -0500

    plus on prices 20230102

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -13,17 +13,17 @@ const Services = () => {
       <div className='servicesColor'>
         <div className='colorTextBox'>
           <h2>COLOR</h2><br />
-          <h4 className='colorLabel'>Highlights: <br /> <span>$325 <BiRightArrow size="15"/> 3hr</span></h4>
+          <h4 className='colorLabel'>Highlights: <br /> <span>$325+ <BiRightArrow size="15"/> 3hr</span></h4>
           <p className='colorText'>Highlighting techniques include foiling and free-hand painting. Service includes toner/gloss and blow dry.</p><br />
           <h4 className='colorLabel'>Single Process: <br /> <span>$150+ <BiRightArrow size="15"/> 2hr</span></h4>
           <p className='colorText'>Service includes a root touch up for color or gray coverage, toner/gloss and blow dry.</p><br />
-          <h4 className='colorLabel'>All Over Color: <br /> <span>$200+ <BiRightArrow size="15"/> 2hr+</span></h4>
+          <h4 className='colorLabel'>All Over Color: <br /> <span>$200+ <BiRightArrow size="15"/> 2hr</span></h4>
           <p className='colorText'>Service includes all-over hair color application that covers grays or creates a holistic color change, toner/gloss and blow dry.</p><br />
           <h4 className='colorLabel'>Gloss | Toner | Glaze: <br /> <span>$85+ <BiRightArrow size="15"/> 1hr</span></h4>
           <p className='colorText'>Service includes toning/color melt/gloss and blow dry.</p><br />
-          <h4 className='colorLabel'>Double Process Retouch: <br /> <span>$225+ <BiRightArrow size="15"/> 2hr+</span></h4>
+          <h4 className='colorLabel'>Double Process Retouch: <br /> <span>$225+ <BiRightArrow size="15"/> 2hr</span></h4>
           <p className='colorText'>This is a four-to-six-week refresh for existing, double-process blondes. Service includes toner/gloss and blow dry.</p><br />
-          <h4 className='colorLabel'>Color Correction | Complete Color Change: <br /> <span>$325+ <BiRightArrow size="15"/> 3hr+</span></h4>
+          <h4 className='colorLabel'>Color Correction | Complete Color Change: <br /> <span>$325+ <BiRightArrow size="15"/> 3hr</span></h4>
         </div>
         <div className='colorImgBox'>
           <img className='servicesColorImg' src="https://i.imgur.com/22KS9Yc.jpg" alt="photo2" />
@@ -35,13 +35,13 @@ const Services = () => {
         </div>
         <div className='stylingTextBox'>
           <h2>STYLING</h2><br />
-          <h4 className='stylingLabel'>Haircut: <br /> <span>$115 <BiRightArrow size="15"/> 1hr 15min</span></h4>
+          <h4 className='stylingLabel'>Haircut: <br /> <span>$115+ <BiRightArrow size="15"/> 1hr 15min</span></h4>
           <p className='stylingText'>Service includes blow dry/style.</p><br />
-          <h4 className='stylingLabel'>Blow Dry: <br /> <span>$70 <BiRightArrow size="15"/> 45min</span></h4>
+          <h4 className='stylingLabel'>Blow Dry: <br /> <span>$70+ <BiRightArrow size="15"/> 45min</span></h4>
           <br />
           <h4 className='stylingLabel'>Up do | Special Occasion: <br /> <span>$95+ <BiRightArrow size="15"/> 1hr</span></h4>
           <br />
-          <h4 className='stylingLabel'>Men's Haircut: <br /> <span>$55 <BiRightArrow size="15" /> 1hr</span></h4>
+          <h4 className='stylingLabel'>Men's Haircut: <br /> <span>$55+ <BiRightArrow size="15" /> 1hr</span></h4>
           <br />
           <h2>EXTENSIONS</h2>
           <p className='stylingText'>All extension services are priced upon consultation</p><br />
