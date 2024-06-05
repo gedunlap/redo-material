b68ae03d976a5e5e7f430b0d7e92036c121d4125
gedunlap <g.e.dunlap90@gmail.com>
2024-06-05 09:27:34 -0400

    change suite

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -26,7 +26,7 @@ const Home = () => {
           <br />
           <div><AiOutlinePhone size='35' style={{color:"white"}} /><br /><a href="tel:+1-813-280-1356" style={{color:"white", textDecoration:"none", fontSize:"25px"}}>(813) 280-1356</a></div>
           <br />
-          <div className="t4"><p>Suite A 2502 N. Howard Ave Suite B Tampa, FL 33607</p></div>
+          <div className="t4"><p>2502 N. Howard Ave Suite A Tampa, FL 33607</p></div>
           <div className="t5">Tues: 10am-5pm</div>
           <div className='t5'>Tuesday - Saturday: 9am-5pm</div>
           <div className='t6'> Sat: 10am-4pm</div>
